006b46164ea70021aa4cb75c4bd655668aeb4699
dougfoo <doug.cha@gmail.com>
2019-07-01 21:56:04 -0400

    AB#20 initial django jWT backend"

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
 from .views import UserViewSet, LoanViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'loans', LoanViewSet)
 
 urlpatterns = [
+    re_path(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
--- server/api/views.py
+++ server/api/views.py
@@ -2,20 +2,14 @@ from django.shortcuts import render
 
 from .models import User, Loan
 from .serializers import UserSerializer, LoanSerializer
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
-
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing user instances.
-    """
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 class LoanViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing user instances.
-    """
+    permission_classes = (IsAuthenticated,)
     serializer_class = LoanSerializer
     queryset = Loan.objects.all()
