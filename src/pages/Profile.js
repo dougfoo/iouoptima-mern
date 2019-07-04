c0b974494ba429a00502e726f20050678722d791
dougfoo <doug.cha@gmail.com>
2019-07-03 23:15:55 -0400

    AB#17 small changes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -10,13 +10,14 @@ class Profile extends Component {
       <React.Fragment>
         <h1>Profile</h1>
 
-        <Descriptions title="User Info" layout="vertical">
-          <Descriptions.Item label="id">{this.props.activeUser["id"]}</Descriptions.Item>
+        <Descriptions title="User Info" border
+        column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }} >
           <Descriptions.Item label="email">{this.props.activeUser["email"]}</Descriptions.Item>
           <Descriptions.Item label="First">{this.props.activeUser["firstName"]}</Descriptions.Item>
           <Descriptions.Item label="Last">{this.props.activeUser["lastName"]}</Descriptions.Item>
           <Descriptions.Item label="phone">{this.props.activeUser["phone"]}</Descriptions.Item>
           <Descriptions.Item label="friends">{this.props.activeUser["friends"]}</Descriptions.Item>
+          <Descriptions.Item label="id">{this.props.activeUser["id"]}</Descriptions.Item>
         </Descriptions> 
       </React.Fragment>
      )
