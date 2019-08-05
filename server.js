a9aaa0122501a07c6f8870846916c958a4f72874
dougfoo <doug.cha@gmail.com>
2019-08-04 20:31:04 -0400

    trying bear example

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
-
+mongoose.connect('mongodb://admin:admin99@ds027729.mlab.com:27729/iouexpress'); // mlab free db
+var Bear     = require('./src/server/bear');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -23,10 +23,38 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// --- bear routes start
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+// more routes for our API will happen here
+// on routes that end in /bears
+// ----------------------------------------------------
+
+router.route('/bears')
+    .post(function(req, res) {
+        console.log('post request.');
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+        // save the bear and check for errors
+        bear.save(function(err) {
+            console.log('bear save');
+            if (err)
+                res.send(err);
+            res.json({ message: 'Bear created!' });
+        });
+    });
+
+// --- bear routes end
 
 // more routes for our API will happen here
 
